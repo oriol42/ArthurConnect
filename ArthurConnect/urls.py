0701a828ec229240e711489b7919abddfd961c0f
njilo1 <alphaneo12@gmail.com>
2025-08-04 18:13:55 +0200

    page configuere fonctionelle

--- ArthurConnect/urls.py
+++ ArthurConnect/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
   path('home',views.index,name='index'),
+  path('category',views.category,name='category'),
+  path('contact',views.contact,name='contact'),
+  path('cart',views.cart,name='cart'),
+  path('checkout',views.checkout,name='checkout'),
+  path('singleproduct',views.singleproduct,name='singleproduct'),
 ]
--- ArthurConnect/views.py
+++ ArthurConnect/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
   return render(request,'ArthurConnect/index.html')
+
+def category(request):
+  return render(request,'ArthurConnect/category.html')
+
+def contact(request):
+  return render(request,'ArthurConnect/contact.html')
+
+def cart(request):
+  return render(request,'ArthurConnect/cart.html')
+
+def checkout(request):
+  return render(request,'ArthurConnect/checkout.html')
+
+def singleproduct(request):
+  return render(request,'ArthurConnect/single-product.html')
